c37195393ce8c7864067b07abc54998af304ee33
KateTurton <rinekate@hotmail.com>
2020-10-06 00:36:57 +0100

    Create CO452Lab.java

---
+++ app01-lab-classes/CO452Lab.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class CO452Lab here.
+ *
+ * @author (Kate Turton)
+ * @version (05/10/2020)
+ */
+public class CO452Lab
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class CO452Lab
+     */
+    public CO452Lab()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}

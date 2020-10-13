a0b8aa2256ac141c6c1d2a67b5b665ac1ac6f29c
KateTurton <rinekate@hotmail.com>
2020-10-13 11:50:02 +0100

    Create Ticket.java

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,39 @@
+
+/**
+ * Create code for destinations under method to assign each location within objects.
+ *
+ * @author Kate Turton
+ * @version 13/10/2020
+ */
+public class Ticket
+{
+    // instance variables - replace the example below with your own
+    private String destination;
+    
+    private int price;
+    
+    private String date;
+
+    /**
+     * Constructor for objects of class Ticket
+     */
+    public Ticket(String destination, int price, String date)
+    {
+        // initialise instance variables
+        this.destination = destination;
+        this.price = price;
+        this.date = date;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod()
+    {
+        // put your code here
+        
+    }
+}

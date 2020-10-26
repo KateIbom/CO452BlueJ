c8b68f484defce43343c93116060d751a266993e
KateTurton <rinekate@hotmail.com>
2020-10-26 20:41:18 +0000

    created constructor adding module to course

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -11,6 +11,11 @@ public class Course
     private String moduleNumber;
     // Course Title
     private String Title;
+    
+    private Module module1;
+    private Module module2;
+    private Module module3;
+    private Module module4;
 
     /**
      * Constructor for objects of class Course
@@ -20,7 +25,40 @@ public class Course
         this.moduleNumber = moduleNumber;
         this.Title = title;
     }
-
+    
+    
+    /**
+     * Constructor to insert module unto course
+     */
+    
+    public void addModule(Module module, int moduleNo)
+    {
+        if (moduleNo == 1)
+        {
+            this.module1 = module;
+        }
+        
+        else if (moduleNo == 2)
+        {
+            this.module2 = module;
+        }
+        
+        else if (moduleNo == 3)
+        {
+            this.module3 = module;
+        }
+        
+        else if (moduleNo == 4)
+        {
+            this.module4 = module;
+        }
+        
+    
+    }
+    
+    
+    
+    
     /**
      * AMethod that returns module number
      *

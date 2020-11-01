9e30b352bf466839c82b7bb91493101a61ecd761
KateTurton <rinekate@hotmail.com>
2020-11-01 14:34:12 +0000

    updated

--- app04-products/Product.java
+++ app04-products/Product.java
@@ -1,8 +1,8 @@
 /**
  * Model some details of a product sold by a company.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Kate Turton.
+ * @version 27/10/2020
  */
 public class Product
 {
